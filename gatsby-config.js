139b6d85ba69a0387be9b63e7d7bf5a5a03628d2
pranay <drpranayaryal@gmail.com>
2020-07-21 23:49:32 -0400

    remove google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,24 +61,6 @@ module.exports = {
       options: {
         postCssPlugins: [require('postcss-color-function'), require('cssnano')()],
       },
-    },
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: 'UA-58522655-4',
-        // Puts tracking script in the head instead of the body
-        head: true,
-        // IP anonymization for GDPR compliance
-        anonymize: true,
-        // Disable analytics for users with `Do Not Track` enabled
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**'],
-        // Specifies what percentage of users should be tracked
-        sampleRate: 100,
-        // Determines how often site speed tracking beacons will be sent
-        siteSpeedSampleRate: 10,
-      },
-    },
+    }
   ],
 };
